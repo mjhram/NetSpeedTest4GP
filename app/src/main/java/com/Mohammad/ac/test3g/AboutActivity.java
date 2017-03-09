c65b8772171effd718fab03d9c7963a9688ee5ff
Mohammad J. Haider <mjhram2000@gmail.com>
2017-03-09 12:18:02 +0300

    add actionbar for about&options

--- app/src/main/java/com/Mohammad/ac/test3g/AboutActivity.java
+++ app/src/main/java/com/Mohammad/ac/test3g/AboutActivity.java
@@ -1,17 +1,22 @@
 package com.Mohammad.ac.test3g;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class AboutActivity
-  extends Activity
+  extends AppCompatActivity
 {
   protected void onCreate(Bundle paramBundle)
   {
     super.onCreate(paramBundle);
     setContentView(R.layout.activity_about);
+
+    Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
   }
   
   public boolean onCreateOptionsMenu(Menu paramMenu)
